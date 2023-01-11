6bb208807b2dc2f3d3baa6300ed1b91b37e9b496
OvermindMC <Overmind@mail.com>
2023-01-11 20:39:26 +0000

    Update Module.h

--- Flarial/Client/Manager/Category/Module/Module.h
+++ Flarial/Client/Manager/Category/Module/Module.h
@@ -2,14 +2,40 @@
 #include "../Category.h"
 #include "../../Event/Event.h"
 
+struct ModuleEventArgs {
+    bool isEnabled, isTicking;
+};
+
 class Module {
 public:
 	Category* category = nullptr;
-	Events* events = nullptr;
+	std::vector<__int64*> events;
+public:
+	std::string name;
 public:
 	bool wasEnabled = false, isEnabled = false;
 public:
-	Module(Category*);
+	Module(Category*, std::string);
 public:
 	auto tick(void) -> void;
+public:
+    template<typename T>
+    auto registerEvent(std::function<void(T*)> fn) {
+        auto ev = new Event<T>();
+
+        ev->type = typeid(T).name();
+        ev->callback = fn;
+
+        this->events.push_back((__int64*)ev);
+    };
+public:
+    template<typename T>
+    auto callEvent(T args) {
+        for (auto ptr : this->events) {
+            auto ev = (Event<T>*)ptr;
+            
+            if (typeid(T).name() == ev->type)
+                ev->callback(&args);
+        };
+    };
 };
