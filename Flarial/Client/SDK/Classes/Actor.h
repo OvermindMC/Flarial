fedae52c3592f24530e091546a6ad79e058aae54
OvermindMC <Overmind@mail.com>
2023-01-11 20:39:22 +0000

    Update Actor.h

--- Flarial/Client/SDK/Classes/Actor.h
+++ Flarial/Client/SDK/Classes/Actor.h
@@ -5,6 +5,8 @@
 class Actor {
 public:
 	BUILD_ACCESS(this, bool, isOnGround, 0x188);
+public:
+	BUILD_ACCESS(*(uintptr_t**)((uintptr_t)(this) + 0x350), Vec3<float>, motion, 0x18);
 private:
 	virtual ~Actor(void);
 public:
