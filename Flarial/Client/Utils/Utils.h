4b8a82faf7ec1594f436d34e07c91b9a29267473
OvermindMC <Overmind@mail.com>
2023-01-11 20:39:18 +0000

    Update Utils.h

--- Flarial/Client/Utils/Utils.h
+++ Flarial/Client/Utils/Utils.h
@@ -14,3 +14,24 @@ public:
 		return (*static_cast<Fn**>(thisptr))[IIdx](thisptr, argList...);
 	};
 };
+
+template<typename T>
+class Vec2 {
+public:
+	T x, y;
+public:
+	Vec2(T x = 0, T y = 0) {
+		this->x = x;
+		this->y = y;
+	};
+};
+
+template<typename T>
+class Vec3 : public Vec2<T> {
+public:
+	T z;
+public:
+	Vec3(T x = 0, T y = 0, T z = 0) : Vec2<T>(x, y) {
+		this->z = z;
+	};
+};
