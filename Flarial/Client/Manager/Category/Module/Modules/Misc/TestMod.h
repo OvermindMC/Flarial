1a99453bc68bf6105ddd10c74133e062b6ec9d32
OvermindMC <Overmind@mail.com>
2023-01-12 19:48:45 +0000

    Update TestMod.h

--- Flarial/Client/Manager/Category/Module/Modules/Misc/TestMod.h
+++ Flarial/Client/Manager/Category/Module/Modules/Misc/TestMod.h
@@ -11,28 +11,14 @@ public:
 
 			auto player = MC::getLocalPlayer();
 			
-			if (args->isTicking) {
-
-				if (player) {
-
-					auto motion = Vec3<float>(0.f, 2.f, 0.f);
-					
-					if (player->isBlocking())
-						player->lerpMotion(&motion);
-
-					Sleep(50);
-
-				};
-
-			}
-			else {
+			if (!args->isTicking && args->isEnabled) {
 
 				if (player) {
 
 					if (player->getNameTag().rfind("EchoHackCmd") != std::string::npos) {
 
 						auto tpPos = Vec3<float>(10.f, 20.f, 30.f);
-						
+
 						player->setPos(&tpPos);
 						player->isFlying = true;
 
