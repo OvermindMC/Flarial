40a4a81522ff1d53953f362a3b655993e1d6ed2b
OvermindMC <Overmind@mail.com>
2023-01-14 08:28:54 +0000

    Update Manager.cpp

--- Flarial/Client/Manager/Manager.cpp
+++ Flarial/Client/Manager/Manager.cpp
@@ -57,7 +57,7 @@ Manager::Manager(Client* c) {
 
 	MH_Initialize();
 
-	auto renderCtxHook = new Hook<void, uintptr_t*, uintptr_t*>(
+	new Hook<void, uintptr_t*, uintptr_t*>(
 		this,
 		"RenderContext",
 		Mem::findSig("48 8B C4 48 89 58 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B ? 48 89 54 24 ? 4C"),
@@ -70,6 +70,24 @@ Manager::Manager(Client* c) {
 				/* Draw stuff via ctx here */
 
 				_this->_Func(p1, ctx);
+
+			};
+
+		}
+	);
+
+	new Hook<void, uint64_t, bool>(
+		this,
+		"KeyHook",
+		Mem::findSig("48 ? ? 48 ? ? ? 4C 8D 05 ? ? ? ? 89"),
+		[&](uint64_t key, bool isDown) {
+
+			auto _this = getHook<void, uint64_t, bool>("KeyHook");
+
+			if(_this) {
+
+				if (key != VK_ESCAPE)
+					_this->_Func(key, isDown);
 
 			};
 
