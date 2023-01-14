d099947d252c3028312c7d8e92b05142c35bb302
OvermindMC <Overmind@mail.com>
2023-01-14 16:28:29 +0000

    Update ClientInstance.h

--- Flarial/Client/SDK/Classes/ClientInstance.h
+++ Flarial/Client/SDK/Classes/ClientInstance.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "Player.h"
+#include "GuiData.h"
 #include "MinecraftGame.h"
 
 class ClientInstance {
 public:
 	auto getPlayer(void) -> LocalPlayer* {
 		return Utils::CallVFunc<24, LocalPlayer*>(this);
+	};
+public:
+	auto getGuiData(void) -> GuiData* {
+		return *(GuiData**)((uintptr_t)(this) + 0x500);
 	};
 public:
 	auto getMcGame(void) -> MinecraftGame* {
