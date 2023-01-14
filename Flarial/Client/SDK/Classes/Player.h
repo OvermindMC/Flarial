d58dbfccc0be10e0ea9ef1f907d95eef8ee9d7e1
OvermindMC <Overmind@mail.com>
2023-01-14 17:05:49 +0000

    Update Player.h

--- Flarial/Client/SDK/Classes/Player.h
+++ Flarial/Client/SDK/Classes/Player.h
@@ -5,5 +5,9 @@ class LocalPlayer : public Actor {
 private:
 	virtual ~LocalPlayer(void);
 public:
-	//
+	auto setSprinting(bool state) -> void {
+
+		return Utils::CallVFunc<284, void, bool>(this, state);
+
+	};
 };
