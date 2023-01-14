db4086ebded2ff14c0cfc631b820dc42c963f9b2
OvermindMC <Overmind@mail.com>
2023-01-14 08:41:14 +0000

    Update Manager.cpp

--- Flarial/Client/Manager/Manager.cpp
+++ Flarial/Client/Manager/Manager.cpp
@@ -1,40 +1,5 @@
 #include "Manager.h"
-
-template<typename T, typename... TArgs>
-class Hook {
-public:
-	Manager* manager = nullptr;
-public:
-	using Func = T(__thiscall*)(TArgs...);
-	Func _Func = (Func)nullptr;
-public:
-	static T detourCallback(TArgs... args) {
-		if(callback)
-			callback(args...);
-	};
-public:
-	static inline std::function<T(TArgs...)> callback = [&](TArgs... args) -> T {};
-	__int64* sig;
-public:
-	const char* name;
-public:
-	Hook(Manager* mgr, const char* name, uintptr_t sig, std::function<T(TArgs...)> cb) {
-
-		this->name = name;
-		this->manager = mgr;
-		this->callback = cb;
-		this->sig = (__int64*)sig;
-
-		if (MH_CreateHook((void*)sig, &detourCallback, reinterpret_cast<LPVOID*>(&_Func)) != MH_OK) {
-			Utils::debugOutput("Failed to initialize " + std::string(name) + " hook!");
-			return;
-		};
-
-		MH_EnableHook((void*)sig);
-		this->manager->hooks.push_back((__int64*)this);
-
-	};
-};
+#include "Hook/Hook.h"
 
 /* Module Includes */
 
