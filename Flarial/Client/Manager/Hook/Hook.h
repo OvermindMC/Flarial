0f9dfb36af245fe1fe80079c8a931a8055be3d0d
OvermindMC <Overmind@mail.com>
2023-01-14 08:41:16 +0000

    Update Hook.h

--- Flarial/Client/Manager/Hook/Hook.h
+++ Flarial/Client/Manager/Hook/Hook.h
@@ -9,22 +9,30 @@ public:
 	using Func = T(__thiscall*)(TArgs...);
 	Func _Func = (Func)nullptr;
 public:
-	std::function<T(TArgs...)> callback;
+	static T detourCallback(TArgs... args) {
+		if (callback)
+			callback(args...);
+	};
+public:
+	static inline std::function<T(TArgs...)> callback = [&](TArgs... args) -> T {};
 	__int64* sig;
 public:
-	Hook(Manager* mgr, const char* name, unsigned long long sig, std::function<T(TArgs...)> cb) {
-		
-		this->sig = sig;
+	const char* name;
+public:
+	Hook(Manager* mgr, const char* name, uintptr_t sig, std::function<T(TArgs...)> cb) {
+
 		this->name = name;
 		this->manager = mgr;
 		this->callback = cb;
+		this->sig = (__int64*)sig;
 
-		if (MH_CreateHook((void*)sig, &cb, reinterpret_cast<LPVOID*>(&_Func)) != MH_OK) {
-			Utils::debugOutput("Failed to initialize " + std::string(name) " hook!");
+		if (MH_CreateHook((void*)sig, &detourCallback, reinterpret_cast<LPVOID*>(&_Func)) != MH_OK) {
+			Utils::debugOutput("Failed to initialize " + std::string(name) + " hook!");
 			return;
 		};
 
 		MH_EnableHook((void*)sig);
+		this->manager->hooks.push_back((__int64*)this);
 
 	};
 };
