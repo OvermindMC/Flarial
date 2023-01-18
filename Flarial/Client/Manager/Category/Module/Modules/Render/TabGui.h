5bfcc2fbd695003760585231e98da1b918b2cf41
OvermindMC <Overmind@mail.com>
2023-01-18 19:22:16 +0000

    Update TabGui.h

--- Flarial/Client/Manager/Category/Module/Modules/Render/TabGui.h
+++ Flarial/Client/Manager/Category/Module/Modules/Render/TabGui.h
@@ -7,6 +7,7 @@ public:
 	int currCategory = 0, currModule = 0;
 public:
 	float alpha = 0.f;
+	int renderFrame = 0;
 public:
 	TabGui(Manager* mgr) : Module(mgr->categories[CategoryType::RENDER], "TabGui") {
 
@@ -14,16 +15,29 @@ public:
 
 		registerEvent<RenderCtxEvent>([&](RenderCtxEvent* args) {
 
+			this->renderFrame++;
+			
+			if(this->renderFrame < 3)
+				return;
+
+			this->renderFrame = 0;
+
 			auto instance = args->ctx->instance;
-			auto font = (instance != nullptr && instance->getMcGame() != nullptr ? instance->getMcGame()->mcFont : nullptr);
+			auto mcGame = (instance != nullptr ? instance->getMcGame() : nullptr);
+			auto font = (mcGame != nullptr ? mcGame->mcFont : nullptr);
 
 			if (instance == nullptr || font == nullptr)
+				return;
+
+			Utils::reachOff(&alpha, (mcGame->canUseKeys ? 1.f : 0.f), 0.05f);
+			
+			if (alpha <= 0.f)
 				return;
 			
 			auto mgr = this->category->manager;
 			auto client = mgr->client;
 
-			args->ctx->drawText(font, client->name, Vec2<float>(10.f, 10.f), Color(255.f, 255.f, 255.f), 1.f);
+			args->ctx->drawText(font, client->name, Vec2<float>(10.f, 10.f), Color(255.f, 255.f, 255.f, alpha), 1.f);
 			args->ctx->flushText(0.f);
 
 		});
