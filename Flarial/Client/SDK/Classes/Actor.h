461b9199b8f2c15ad4c82b9d059fe64027d334d9
OvermindMC <Overmind@mail.com>
2023-01-20 23:05:08 +0000

    Update Actor.h

--- Flarial/Client/SDK/Classes/Actor.h
+++ Flarial/Client/SDK/Classes/Actor.h
@@ -14,6 +14,8 @@ public:
 	BUILD_ACCESS(*(uintptr_t**)((uintptr_t)(this) + 0x350), Vec3<float>, motion, 0x18);
 	BUILD_ACCESS(*(uintptr_t**)((uintptr_t)(this) + 0x360), Vec3<float>, bodyRot, 0x0);
 	BUILD_ACCESS(*(uintptr_t**)((uintptr_t)(this) + 0x358), AABB<float>, boundingBox, 0x0);
+public:
+	auto getHealth(void) -> int;
 private:
 	virtual ~Actor(void);
 public:
@@ -233,4 +235,189 @@ public:
 	virtual auto handleEntityEvent(enum ActorEvent, int) -> void;
 public:
 	virtual auto getPickRadius(void) -> float;
+public:
+	virtual auto getActorRendererId(void) -> class HashedStruct*;
+public:
+	virtual auto spawnAtLocation(class ItemStack*, float) -> class ItemActor*;
+	virtual auto spawnAtLocation(class Block*, int, float) -> class ItemActor*;
+	virtual auto spawnAtLocation(class Block*, int) -> class ItemActor*;
+	virtual auto spawnAtLocation(int, int, float) -> class ItemActor*;
+	virtual auto spawnAtLocation(int, int) -> class ItemActor*;
+public:
+	virtual auto despawn(void) -> void;
+	virtual auto killed(Actor*) -> void;
+	virtual auto awardKillScore(Actor*, int) -> void;
+public:
+	virtual auto setArmor(enum ArmorSlot, class ItemStack const&) -> void;
+public:
+	virtual auto getArmor(enum ArmorSlot) -> void;
+	virtual auto getAllArmor(void) -> std::vector<class ItemStack*>;
+public:
+	virtual auto getArmorMaterialTypeInSlot(enum ArmorSlot) -> enum ArmorMaterialType;
+	virtual auto getArmorMaterialTextureTypeInSlot(enum ArmorSlot) -> enum ArmorTextureType;
+	virtual auto getArmorColorInSlot(enum ArmorSlot, int) -> float;
+public:
+	virtual auto getEquippedSlot(enum EquipmentSlot) -> class ItemStack const&;
+	virtual auto setEquippedSlot(enum EquipmentSlot, class ItemStack const&) -> void;
+public:
+	virtual auto setCarriedItem(class ItemStack*) -> void;
+	virtual auto getCarriedItem(void) -> class ItemStack*;
+	virtual auto setOffhandSlot(class ItemStack*) -> void;
+	virtual auto getEquippedTotem(void) -> class ItemStack*;
+	virtual auto consumeTotem(void) -> void;
+public:
+	virtual auto save(class CompoundTag*) -> bool;
+	virtual auto saveWithoutId(class CompoundTag*) -> void;
+	virtual auto load(class CompoundTag*, class DataLoadHelper*) -> bool;
+	virtual auto loadLinks(class CompoundTag const&, class std::vector<class ActorLink, class std::allocator<class ActorLink>>&, class DataLoadHelper&) -> void;
+public:
+	virtual auto getEntityTypeId(void) -> enum ActorType;
+};
+
+enum ActorType {
+
+	/* Drops */
+
+	item = 64,
+	xp_orb = 69,
+
+	/* Blocks */
+
+	tnt = 65,
+	falling_block = 66,
+	moving_block = 67,
+
+	/* Immobile and projectiles */
+
+	armor_stand = 61,
+	xp_bottle = 68,
+	eye_of_ender_signal = 70,
+	ender_crystal = 71,
+	fireworks_rocket = 72,
+	thrown_trident = 73,
+	shulker_bullet = 76,
+	fishing_hook = 77,
+	dragon_fireball = 79,
+	arrow = 80,
+	snowball = 81,
+	egg = 82,
+	painting = 83,
+	minecart = 84,
+	fireball = 85,
+	splash_potion = 86,
+	ender_pearl = 87,
+	leash_knot = 88,
+	wither_skull = 89,
+	boat = 90,
+	wither_skull_dangerous = 91,
+	lightning_bolt = 93,
+	small_fireball = 94,
+	area_effect_cloud = 95,
+	hopper_minecart = 96,
+	tnt_minecart = 97,
+	chest_minecart = 98,
+	command_block_minecart = 100,
+	lingering_potion = 101,
+	llama_spit = 102,
+	evocation_fang = 103,
+	ice_bomb = 106,
+	balloon = 107,
+	chest_boat = 218,
+
+	/* Hostile mobs */
+
+	zombie = 32,
+	creeper = 33,
+	skeleton = 34,
+	spider = 35,
+	zombie_pigman = 36,
+	slime = 37,
+	enderman = 38,
+	silverfish = 39,
+	cave_spider = 40,
+	ghast = 41,
+	magma_cube = 42,
+	blaze = 43,
+	zombie_villager = 44,
+	witch = 45,
+	stray = 46,
+	husk = 47,
+	wither_skeleton = 48,
+	guardian = 49,
+	elder_guardian = 50,
+	wither = 52,
+	ender_dragon = 53,
+	shulker = 54,
+	endermite = 55,
+	vindicator = 57,
+	phantom = 58,
+	ravager = 59,
+	evocation_illager = 104,
+	vex = 105,
+	drowned = 110,
+	pillager = 114,
+	zombie_villager_v2 = 116,
+	piglin = 123,
+	hoglin = 124,
+	zoglin = 126,
+	piglin_brute = 127,
+	warden = 131,
+
+	/* Passive and neutral mobs */
+
+	chicken = 10,
+	cow = 11,
+	pig = 12,
+	sheep = 13,
+	wolf = 14,
+	villager = 15,
+	mooshroom = 16,
+	squid = 17,
+	rabbit = 18,
+	bat = 19,
+	iron_golem = 20,
+	snow_golem = 21,
+	ocelot = 22,
+	horse = 23,
+	donkey = 24,
+	mule = 25,
+	skeleton_horse = 26,
+	zombie_horse = 27,
+	polar_bear = 28,
+	llama = 29,
+	parrot = 30,
+	dolphin = 31,
+	turtle = 74,
+	cat = 75,
+	pufferfish = 108,
+	salmon = 109,
+	tropicalfish = 111,
+	cod = 112,
+	panda = 113,
+	villager_v2 = 115,
+	wandering_trader = 118,
+	fox = 121,
+	bee = 122,
+	strider = 125,
+	goat = 128,
+	frog = 132,
+	tadpole = 133,
+	allay = 134,
+
+	/* Other */
+
+	player = 63,
+	shield = 117,
+
+	/* Unused */
+
+	elder_guardian_ghost = 120,
+
+	/* Education Edition */
+
+	npc = 51,
+	agent = 56,
+	tripod_camera = 62,
+	chalkboard = 78
+
 };
