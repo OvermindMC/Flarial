843807305c718fd0f1d739fb82b21f7ffde8a0ee
OvermindMC <Overmind@mail.com>
2023-01-20 23:57:35 +0000

    Update Manager.cpp

--- Flarial/Client/Manager/Manager.cpp
+++ Flarial/Client/Manager/Manager.cpp
@@ -27,7 +27,7 @@ Manager::Manager(Client* c) {
 
 	MH_Initialize();
 
-	{
+	{ /* Draw Image */
 
 		auto sig = Mem::findSig("48 8D 05 ? ? ? ? 48 89 01 48 89 51 ? 4C 89 41 ? 48 8B 02");
 
@@ -74,64 +74,122 @@ Manager::Manager(Client* c) {
 
 	};
 
-	new Hook<void, uintptr_t*, MinecraftUIRenderContext*>(this, "RenderContext", Mem::findSig("48 8B C4 48 89 58 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B ? 48 89 54 24 ? 4C"),
-		[&](uintptr_t* p1, MinecraftUIRenderContext* ctx) {
+	{ /* Entity */
 
-			auto _this = getHook<void, uintptr_t*, MinecraftUIRenderContext*>("RenderContext");
-			
-			if(_this) {
+		auto sig = Mem::findSig("48 8D 05 ? ? ? ? 48 89 01 48 8B 05 ? ? ? ? FF 15 ? ? ? ? 4C 8B C0");
 
-				for (auto [type, category] : this->categories) {
+		if (sig) {
 
-					for (auto mod : category->modules) {
+			auto offset = *(int*)(sig + 3);
+			auto VTable = (uintptr_t**)(sig + offset + 7);
 
-						if (mod->isEnabled)
-							mod->callEvent<RenderCtxEvent>(RenderCtxEvent{ ctx });
-					};
-				};
+			new Hook<void, Actor*>(this, "ActorBaseTick", (uintptr_t)VTable[51], [&](Actor* entity) {
 
-				_this->_Func(p1, ctx);
+				auto _this = getHook<void, Actor*>("ActorBaseTick");
+				
+				if(_this) {
 
-			};
+					this->entityMap[entity->runtimeId] = entity;
 
-		}
-	);
-
-	new Hook<void, uint64_t, bool>(this, "KeyHook", Mem::findSig("48 ? ? 48 ? ? ? 4C 8D 05 ? ? ? ? 89"),
-		[&](uint64_t key, bool isDown) {
-
-			auto cancel = false;
-			auto _this = getHook<void, uint64_t, bool>("KeyHook");
-			
-			if(_this) {
-
-				this->keyMap[key] = isDown;
-
-				for (auto [type, category] : this->categories) {
-
-					for (auto mod : category->modules) {
-
-						if (mod->isEnabled)
-							mod->callEvent<KeyEvent>(KeyEvent{ key, isDown, &cancel });
-
-					};
+					_this->_Func(entity);
 
 				};
 
-				if(!cancel)
-					_this->_Func(key, isDown);
+			});
 
-			};
+		};
 
-		}
-	);
+	};
 
-	new Hook<void, LoopbackPacketSender*, Packet*>(this, "LoopbackSend", Mem::findSig("48 89 5C 24 08 57 48 83 EC 20 48 8B D9 48 8B FA 48 8B 49 10"),
+	{ /* Render Context */
+
+		new Hook<void, uintptr_t*, MinecraftUIRenderContext*>(this, "RenderContext", Mem::findSig("48 8B C4 48 89 58 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D A8 ? ? ? ? 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B ? 48 89 54 24 ? 4C"),
+			[&](uintptr_t* p1, MinecraftUIRenderContext* ctx) {
+
+				auto _this = getHook<void, uintptr_t*, MinecraftUIRenderContext*>("RenderContext");
+		
+				if(_this) {
+
+					for (auto [type, category] : this->categories) {
+
+						for (auto mod : category->modules) {
+
+							if (mod->isEnabled)
+								mod->callEvent<RenderCtxEvent>(RenderCtxEvent{ ctx });
+						};
+					};
+
+					_this->_Func(p1, ctx);
+
+				};
+
+			}
+		);
+	};
+
+	{ /* Key Hook */
+
+		new Hook<void, uint64_t, bool>(this, "KeyHook", Mem::findSig("48 ? ? 48 ? ? ? 4C 8D 05 ? ? ? ? 89"),
+			[&](uint64_t key, bool isDown) {
+
+				auto cancel = false;
+				auto _this = getHook<void, uint64_t, bool>("KeyHook");
+			
+				if(_this) {
+
+					this->keyMap[key] = isDown;
+
+					for (auto [type, category] : this->categories) {
+
+						for (auto mod : category->modules) {
+
+							if (mod->isEnabled)
+								mod->callEvent<KeyEvent>(KeyEvent{ key, isDown, &cancel });
+
+						};
+
+					};
+
+					if(!cancel)
+						_this->_Func(key, isDown);
+
+				};
+
+			}
+		);
+
+	};
+
+	{ /* LoopbackPacketSender */
+
+		new Hook<void, LoopbackPacketSender*, Packet*>(this, "LoopbackSend", Mem::findSig("48 89 5C 24 08 57 48 83 EC 20 48 8B D9 48 8B FA 48 8B 49 10"),
 		[&](LoopbackPacketSender* lp, Packet* packet){
 			
-			//
+				auto _this = getHook<void, LoopbackPacketSender*, Packet*>("LoopbackSend");
+				auto cancel = false;
+
+				if(_this) {
+
+					for (auto [ type, category ] : this->categories) {
+
+						for (auto mod : category->modules) {
+
+							if (mod->isEnabled)
+								mod->callEvent<PacketEvent>(PacketEvent{ &cancel, packet });
+
+						};
+
+					};
+
+					if (!cancel)
+						_this->_Func(lp, packet);
+
+				};
 			
-		});
+			}
+		);
+
+	};
 
 	for (;;) {
 
